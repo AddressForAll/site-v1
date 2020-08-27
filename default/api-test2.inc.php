e389002c3e6377fe764fae8816841d1db1f2694a
venezianluis <felipelipe_11@hotmail.com>
2020-08-26 21:14:27 -0300

    implementação com datables

---
+++ default/api-test2.inc.php
@@ -0,0 +1,91 @@
+<section class="main" id="api">
+    <div><h1>API - Resgatar dados</h1></div>
+    <span><strong>Dados: </strong>Cidades</span>
+    <br><label for="scales"><strong>Estado: </strong></label><select id="estados"></select>
+    <br><label for="scales"><strong>Cidade: </strong></label>
+    <select id="cidades"><option selected value='all'>Trazer Tudo</option></select>
+    <br><label for="scales"><strong>Paginar Resultado: </strong></label><input type="checkbox" id="paginar" checked>
+    <br><strong><button onclick="getdata();">Consultar</button></strong>
+    <br>
+    <br>
+    <table id="tabela" class="display" style="display: none;">
+        <thead>
+            <tr>
+                <th>Id IBGE</th>
+                <th>Name</th>
+                <th>State</th>
+                <th>Abbrev3</th>
+                <th>Id Wikidata</th>
+                <th>LEX Label</th>
+                <th>ISO Label</th>
+                <th>DDD</th>
+            </tr>
+        </thead>
+    </table>
+</section>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.flash.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.21/sp-1.1.1/datatables.min.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css"/>
+
+<script>
+    function getdata(){
+        var uf = $("#estados").children("option:selected").val();
+        var city = $("#cidades").children("option:selected").val();
+        var url = "http://addressforall.org/_sql/city";
+        url += uf != "all" ? "?state=eq." + uf : "";
+        url += city != "all" ? "&name=eq." + city : "";
+        $.getJSON(url, function( data ) {
+            //console.log(typeof(data));
+            $('#tabela').show();
+            $('#tabela').DataTable({
+                "bDestroy": true,
+                "dom": 'Bfrtip',
+                "buttons": ['copy', 'csv', 'excel', 'pdf', 'print'],
+                "data" : data,
+                "columns" : [
+                    {"data" : "ibge_id"},
+                    {"data" : "name"},
+                    {"data" : "state"},
+                    {"data" : "abbrev3"},
+                    {"data" : "wikidata_id"},
+                    {"data" : "lexlabel"},
+                    {"data" : "isolabel_ext"},
+                    {"data" : "ddd"}
+                ],
+                "paging": $('#paginar').prop('checked'),
+                "responsive": true
+            });
+        });
+
+    }
+
+    $(document).ready(function(){
+        $.getJSON('http://localhost:3101/dim_state', function(data){
+            var options = "<option selected value='all'>Trazer Tudo</option>";
+            for (var x = 0; x < data.length; x++) {
+                options += '<option value="' + data[x]['state'] + '">' + data[x]['state'] + " -     (" + data[x]['qt_city'] + ')</option>';
+            }
+            $('#estados').html(options);
+        });
+
+        $('#estados').on('change', function (){
+            var estado_selecionado = $("#estados").children("option:selected").val();
+            $.getJSON('http://localhost:3101/city?state=eq.'+estado_selecionado, function(data){
+                var options = "<option selected value='all'>Trazer Tudo</option>";
+                for (var x = 0; x < data.length; x++) {
+                    options += '<option value="' + data[x]['name'] + '">' + data[x]['name'] + '</option>';
+                }
+                $('#cidades').html(options);
+            });
+        });
+    });
+</script>
--- default/api.inc.php
+++ default/api.inc.php
@@ -5,7 +5,6 @@
 <section class="main" id="api">
     <div>
         <h1>API - Resgatar dados</h1>
-        <form method="POST">
             <span><strong>Dados: </strong>Cidades</span>
             <br>
             <label for="scales"><strong>Estado: </strong></label><select id="estados"></select>
@@ -15,8 +14,35 @@
             <br>
             <label for="scales"><strong>Paginar Resultado: </strong></label><input type="checkbox" id="paginar" checked>
             <br>
-            <stron><a href="#" style="text-decoration: none;" onclick="getdata();">Consultar</a></strong>
-        </form> 
+
+            <!-- FILTRO AVANÇADO -->
+            <div>
+            <label for="scales"><strong>Filtro avançado: </strong></label>
+                <select id="filter-field">
+                    <option></option>
+                    <option value="ibge_id">Id Ibge</option>
+                    <option value="name">Name</option>
+                    <option value="state">State</option>
+                    <option value="abbrev3">Abbrev3</option>
+                    <option value="wikidata_id">Id Wikdata</option>
+                    <option value="lexlabel">Lex Label</option>
+                    <option value="isolabel_ext">ISO Label</option>
+                    <option value="ddd">DDD</option>
+                </select>
+
+                <select id="filter-type">
+                    <option value="=">=</option>
+                    <option value="<"><</option>
+                    <option value="<="><=</option>
+                    <option value=">">></option>
+                    <option value=">=">>=</option>
+                    <option value="!=">!=</option>
+                    <option value="like">like</option>
+                </select>
+                <input id="filter-value" type="text" placeholder="Valor para filtrar">
+                <button id="filter-clear">Limpar</button>
+            </div>
+            <stron><button onclick="getdata();">Consultar</button></strong>
     </div>
     <br>
     <div>
@@ -29,11 +55,9 @@
 </section>
 
 <!-- IMPORTA JQUERY -->
-<script
-  src="https://code.jquery.com/jquery-3.5.1.js"
-  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-  crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 <script>
+
     function getdata(){
         var pagination = $('#paginar').prop('checked');
         var uf = $("#estados").children("option:selected").val();
@@ -67,10 +91,53 @@
         document.getElementById("download-json").addEventListener("click", function(){
             table.download("json", "data.json");
         });
+
+        //Define variables for input elements
+        var fieldEl = document.getElementById("filter-field");
+        var typeEl = document.getElementById("filter-type");
+        var valueEl = document.getElementById("filter-value");
+
+        //Custom filter example
+        function customFilter(data){
+            return data;
+        }
+
+        //Trigger setFilter function with correct parameters
+        function updateFilter(){
+            var filterVal = fieldEl.options[fieldEl.selectedIndex].value;
+            var typeVal = typeEl.options[typeEl.selectedIndex].value;
+
+            var filter = filterVal == "function" ? customFilter : filterVal;
+
+            if(filterVal == "function" ){
+                typeEl.disabled = true;
+                valueEl.disabled = true;
+            }else{
+                typeEl.disabled = false;
+                valueEl.disabled = false;
+            }
+
+            if(filterVal){
+                table.setFilter(filter, typeVal, valueEl.value);
+            }
+        }
+
+        //Update filters on value change
+        document.getElementById("filter-field").addEventListener("change", updateFilter);
+        document.getElementById("filter-type").addEventListener("change", updateFilter);
+        document.getElementById("filter-value").addEventListener("keyup", updateFilter);
+
+        //Clear filters on "Clear Filters" button click
+        document.getElementById("filter-clear").addEventListener("click", function(){
+        fieldEl.value = "";
+        typeEl.value = "=";
+        valueEl.value = "";
+        table.clearFilter();
+        });
     }
 
     $(document).ready(function(){
-        $.getJSON('http://addressforall.org/_sql/dim_state', function(data){
+        $.getJSON('http://localhost:3101/dim_state', function(data){
             var options = "<option selected value='all'>Trazer Tudo</option>";
             for (var x = 0; x < data.length; x++) {
                 options += '<option value="' + data[x]['state'] + '">' + data[x]['state'] + " -     (" + data[x]['qt_city'] + ')</option>';
@@ -80,7 +147,7 @@
 
         $('#estados').on('change', function (){
             var estado_selecionado = $("#estados").children("option:selected").val();
-            $.getJSON('http://addressforall.org/_sql/city?state=eq.'+estado_selecionado, function(data){
+            $.getJSON('http://localhost:3101/city?state=eq.'+estado_selecionado, function(data){
                 var options = "<option selected value='all'>Trazer Tudo</option>";
                 for (var x = 0; x < data.length; x++) {
                     options += '<option value="' + data[x]['name'] + '">' + data[x]['name'] + '</option>';
@@ -89,4 +156,5 @@
             });
         });
     });
+
 </script>
