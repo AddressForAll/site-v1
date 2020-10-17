56c54010ca71a1ba30c8f137b3331f5f846e2a80
Peter Krauss <ppkrauss@gmail.com>
2020-10-17 10:14:09 -0300

    ops add api_default draft

--- default/api_default.inc.php
+++ default/api_default.inc.php
@@ -4,7 +4,7 @@
 if (api_uri_global && api_global_req_keys) {
   const preview_domNode = document.getElementById("api_default_preview");
   preview_domNode.appendChild( document.createTextNode(api_global_req_keys.join('\t')) );
-  for (var item in api_uri_global) if (obj.hasOwnProperty(item)) {
+  for (var item in api_uri_global) if (api_uri_global.hasOwnProperty(item)) {
     var text = Object.values(item).join('\t');
     preview_domNode.appendChild( document.createTextNode(text) );
   }
