915273c139c651b911dd71142f04436e6d047730
Peter Krauss <ppkrauss@gmail.com>
2020-10-17 10:03:43 -0300

    add api_default draft

---
+++ default/api_default.inc.php
@@ -0,0 +1,12 @@
+
+<pre id="api_default_preview"></pre>
+<script>
+if (api_uri_global && api_global_req_keys) {
+  const preview_domNode = document.getElementById("api_default_preview");
+  preview_domNode.appendChild( document.createTextNode(api_global_req_keys.join('\t')) );
+  for (var item in api_uri_global) if (obj.hasOwnProperty(item)) {
+    var text = Object.values(item).join('\t');
+    preview_domNode.appendChild( document.createTextNode(text) );
+  }
+} else alert("ERRO NA CARGA DA API, confira a URL utilizada");
+</script>
